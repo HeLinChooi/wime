42e3424454732bf12bcbb70972dd62228b11edbe
alifmazli <alifmazli01@gmail.com>
2023-01-16 22:28:58 +0800

    Edit Will.sol and test script

--- smart_contract/contracts/Will.sol
+++ smart_contract/contracts/Will.sol
@@ -2,22 +2,39 @@
 pragma solidity ^0.8.9;
 
 contract Will {
+    struct Client {
+        address payable pubKey;
+        address privKey;
+    }
+
     struct Beneficiary {
         address payable pubKey;
         uint256 distribution;
     }
+
     Beneficiary public beneficiary;
+    Client public client;
     mapping(address => bool) public isDistributed;
 
     constructor(
+        address payable _clientPubKey,
         address payable _beneficiaryPubKey,
         uint256 _beneficiaryDistribution
     ) {
+        client.pubKey = _clientPubKey;
         beneficiary.pubKey = _beneficiaryPubKey;
         beneficiary.distribution = _beneficiaryDistribution;
     }
 
-    function getBeneficiaries() public view returns (address) {
+    function getBeneficiary() public view returns (address) {
         return beneficiary.pubKey;
+    }
+
+    function distributeAssets() public payable returns (bool) {
+        uint256 totalAssets = msg.sender.balance;
+        uint256 amount = (beneficiary.distribution * totalAssets) / 100;
+        beneficiary.pubKey.transfer(amount);
+        isDistributed[client.pubKey] = true;
+        return isDistributed[client.pubKey];
     }
 }
--- smart_contract/test/will-test.js
+++ smart_contract/test/will-test.js
@@ -2,16 +2,29 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Will", async function () {
+  const _clientPubKey = "0xcd3B766CCDd6AE721141F452C550Ca635964ce71";
   const _beneficiaryPubKey = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
   const _beneficiaryDistribution = "100";
 
-  it("Should return vault password", async () => {
+  it("Should return beneficiary pubKey", async () => {
     const Will = await ethers.getContractFactory("Will");
     const will = await Will.deploy(
+      _clientPubKey,
       _beneficiaryPubKey,
       _beneficiaryDistribution
     );
-    const beneficiaries = await will.getBeneficiaries();
-    expect(beneficiaries).to.equal(_beneficiaryPubKey);
+    const beneficiary = await will.getBeneficiary();
+    expect(beneficiary).to.equal(_beneficiaryPubKey);
+  });
+
+  it("Should transfer assets to beneficiary", async () => {
+    const Will = await ethers.getContractFactory("Will");
+    const will = await Will.deploy(
+      _clientPubKey,
+      _beneficiaryPubKey,
+      _beneficiaryDistribution
+    );
+    const isDistributed = await will.distributeAssets();
+    expect(isDistributed).to.equal(true);
   });
 });
