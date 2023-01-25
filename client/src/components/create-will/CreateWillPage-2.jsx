8affbe034e4956915a3511a76ef3bb431ae5eb7d
He Lin <helinchooi@gmail.com>
2023-01-25 15:22:53 +0800

    Able to deploy contract from FE

--- client/src/components/create-will/CreateWillPage-2.jsx
+++ client/src/components/create-will/CreateWillPage-2.jsx
@@ -30,7 +30,30 @@ const CreateWillPage = () => {
   const API_URL = "http://localhost:8000";
   const onSubmit = async (data) => {
     console.log(data);
-    const message = await fetch(`${API_URL}/create-will`)
+
+    // Contruct request options
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        "ownerPubKey": data.ownerPubKey,
+        "ownerIcNumber": data.ownerIcNumber,
+        "beneficiaries": [
+          {
+            "beneficiaryPubKey": data.address[0],
+            "percentage": data.percentage[0]
+          }
+        ],
+        "validators": [
+          {
+            "validatorPubKey": "0x123456789",
+            "isValidated": false
+          }
+        ],
+        "ownerPrivKey": data.ownerPrivKey
+      }),
+    };
+    const message = await fetch(`${API_URL}/create-will`, requestOptions)
       .then((res) => {
         return res.json();
       })
@@ -45,7 +68,7 @@ const CreateWillPage = () => {
 
   return (
     <>
-      <Container
+      {/* <Container
         maxWidth="sm"
         sx={{
           marginY: 2,
@@ -56,71 +79,112 @@ const CreateWillPage = () => {
       >
         <Box
           sx={{
-            width: 600,
-            height: 300,
+            // width: 600,
+            // height: 300,
             // backgroundColor: "primary.dark",
           }}
-        >
-          <Grid
-            container
-            rowSpacing={2}
-            columnSpacing={{ xs: 1, sm: 2, md: 2 }}
-          >
-            <Grid item xs={12}>
-              <Typography variant="h5" component="h2">
-                Create Will
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <img src={beneficiaryImg} alt="" height="200px" />
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                variant="contained"
-                onClick={() => setBeneficiaryNumber(beneficiaryNumber + 1)}
-              >
-                <AddIcon />
-                <span>Beneficiary</span>
-              </Button>
-            </Grid>
-            {createArrayWithNumbers(beneficiaryNumber).map((index) => (
-              <React.Fragment key={`beneficiary${index}`}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label={"Beneficiary Address " + (index + 1)}
-                    variant="outlined"
-                    {...register(`address.${index}`)}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    fullWidth
-                    id="outlined-basic"
-                    label="Percentage"
-                    variant="outlined"
-                    type="number"
-                    InputProps={{ inputProps: { min: 0, max: 100 } }}
-                    {...register(`percentage.${index}`)}
-                  />
-                </Grid>
-              </React.Fragment>
-            ))}
-            <Grid item xs={12}>
-              <Button
-                fullWidth
-                variant="contained"
-                onClick={handleSubmit(onSubmit)}
-              >
-                Submit
-              </Button>
-            </Grid>
+        > */}
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          p: 2
+        }}
+      // rowSpacing={2}
+      // columnSpacing={{ xs: 1, sm: 2, md: 2 }}
+      >
+        <Grid item xs={12}>
+            <Typography variant="h5" component="h2">
+              Create Will
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <img src={beneficiaryImg} alt="" height="200px" />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+
+          <Grid item xs={12}>
+            <TextField
+              error={!!errors.ownerPubKey}
+              // helperText="This field is required"
+              fullWidth
+              label="Owner Wallet Address"
+              variant="outlined"
+              {...register("ownerPubKey", { required: true })}
+            />
           </Grid>
-        </Box>
-      </Container>
+          <Grid item xs={12}>
+            <TextField
+              error={!!errors.ownerIcNumber}
+              // helperText="This field is required"
+              fullWidth
+              label="Owner Identity Number"
+              variant="outlined"
+              {...register("ownerIcNumber", { required: true })}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              error={!!errors.ownerPrivKey}
+              // helperText="This field is required"
+              fullWidth
+              label="Owner Private Key"
+              variant="outlined"
+              {...register("ownerPrivKey", { required: true })}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container xs={12} md={6} rowSpacing={2}>
+
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              onClick={() => setBeneficiaryNumber(beneficiaryNumber + 1)}
+            >
+              <AddIcon />
+              <span>Beneficiary</span>
+            </Button>
+          </Grid>
+          {createArrayWithNumbers(beneficiaryNumber).map((index) => (
+            <React.Fragment key={`beneficiary${index}`}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  label={"Beneficiary Address " + (index + 1)}
+                  variant="outlined"
+                  {...register(`address.${index}`)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  label="Percentage"
+                  variant="outlined"
+                  type="number"
+                  InputProps={{ inputProps: { min: 0, max: 100 } }}
+                  {...register(`percentage.${index}`)}
+                />
+              </Grid>
+            </React.Fragment>
+          ))}
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={handleSubmit(onSubmit)}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+      {/* </Box>
+      </Container> */}
     </>
   );
 };
